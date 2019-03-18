545b7fa878823c319c4263cdb22d2a3827bdff85
Andres Ortiz <andresdavidortiz@yahoo.com>
2019-03-17 22:11:10 -0400

    Adds basic files for project

---
+++ RpiMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RpiMovieApp
+//
+//  Created by Andres Ortiz on 3/17/19.
+//  Copyright © 2019 Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
