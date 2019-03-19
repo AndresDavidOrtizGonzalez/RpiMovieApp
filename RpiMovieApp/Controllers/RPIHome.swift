1fc9b83457a733470a891c11ec637ff857d85959
Andres Ortiz <andresdavidortiz@yahoo.com>
2019-03-19 11:19:17 -0400

    Creates controller for home view

--- RpiMovieApp/Controllers/RPIHome.swift
+++ RpiMovieApp/Controllers/RPIHome.swift
@@ -9,38 +9,144 @@
 import UIKit
 
 class RPIHome: UITableViewController {
+    
+    //private let headerId = "CustomTableViewHeader"
+    //private let footerId = "CustomTableViewFooter"
+    private let cellId = "CustomMovieTableViewCell"
+    
 
+    var movies = [Movie]()
+    var category : Int?
+    var animationStyle : UITableView.RowAnimation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.category = 1
+        self.animationStyle = .none
+        
+        setupViews()
+        
+        
+        tableView.register(RpiCustomMovieTableCell.self, forCellReuseIdentifier: cellId)
+        fetchData(kind: category!)
     }
+    
+    func fetchData(kind: Int){
+        
+        RPIMovieLoader.shared.fetchMovies(kind: kind){
+            (result: [Movie], success) in
+            if (success){
+                if (self.movies.count > 0){
+                    var indexPathToReload = [IndexPath]()
+                    for cell in self.tableView.visibleCells{
+                        indexPathToReload.append(self.tableView.indexPath(for: cell)!)
+                    }
+                    self.movies = result
+                    self.tableView.reloadRows(at: indexPathToReload, with: self.animationStyle!)
+                }
+                else{
+                    self.movies = result
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    
+    @objc func changeCategory(sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex + 1 > self.category!)
+        {
+            self.animationStyle = UITableView.RowAnimation.left
+        }
+        else
+        {
+            self.animationStyle = UITableView.RowAnimation.right
+        }
+        
+        switch sender.selectedSegmentIndex {
+            case 0:
+                print("Popular")
+                self.category = 1;
+            case 1:
+                print("Top Rated")
+                self.category = 2;
+            case 2:
+                print("Upcoming")
+                self.category = 3;
+            default:
+                print("Popular")
+                self.category = 1;
+        }
+        fetchData(kind:self.category!)
+    }
+    
+    func setupViews(){
+        let items = ["Popular", "Top Rated", "Upcoming"]
+        let customSC = UISegmentedControl(items: items)
+        customSC.selectedSegmentIndex = 0
+        
+        // Set up Frame and SegmentedControl
+        let frame = view.bounds
+        customSC.frame = CGRect(x: frame.minX + 10, y: frame.minY + 5,
+                                width: frame.width - 20, height: 50)
+        
+        
+        // Style the Segmented Control
+        customSC.layer.cornerRadius = 5.0
+        customSC.backgroundColor = .white
+        customSC.tintColor = .blue
+        
+        // Add target action method
+        customSC.addTarget(self, action: #selector(self.changeCategory(sender:)), for: .valueChanged)
+        
+        self.view.addSubview(customSC)
+
+    }
+
+    
+    
+    
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return  self.movies.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomMovieTableViewCell", for: indexPath) as! RpiCustomMovieTableCell
 
-        // Configure the cell...
-
+        let movie = movies[indexPath.row]
+        
+        /*let title = movie["title"] as! String
+        let plot = movie["overview"] as! String
+        let cover = movie["poster_path"] as! String
+        let baseURL = "https://image.tmdb.org/t/p/w500"
+        let url = URL(string: baseURL + cover)
+        let placeholderImage = UIImage(named: "movieplaceholder")!*/
+        
+        //cell.imgMovieCover.af_setImage(withURL: url!, placeholderImage: placeholderImage)
+        
+        cell.imgMoviePoster.image = UIImage.init(named: "movieplaceholder")
+        cell.lblMovieTitle.text = movie.title
+        cell.lblMovieOverview.text = movie.overview
         return cell
     }
-    */
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+ 
 
     /*
     // Override to support conditional editing of the table view.
