d1b0fe96258270f5c02b11692345b3b4ae239d3c
Andres Ortiz <andresdavidortiz@yahoo.com>
2019-03-22 14:51:12 -0400

    Prepares delegate for services

--- RpiMovieApp/AppDelegate.swift
+++ RpiMovieApp/AppDelegate.swift
@@ -13,9 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let reachService = RPIReachability.shared
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
         
         let viewControler = RPIHome()
         let navigationController = UINavigationController(rootViewController: viewControler)
